fe039ed4e8acdd3c711201aee6cf5ec63262e54b
Bikki <32074350+bgeisl16@users.noreply.github.com>
2017-12-22 20:24:16 +0100

    Maa
    Maa p3 maa

---
+++ Client/Client/Source.cpp
@@ -0,0 +1,53 @@
+//Client
+
+#pragma comment(lib, "ws2_32.lib")
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+
+#include <WinSock2.h>
+#include <iostream>
+
+SOCKET Connection;
+
+void ClientThread() {
+	char buffer[256];
+	while (true) {
+		recv(Connection, buffer, sizeof(buffer), NULL);
+		std::cout << buffer << std::endl;
+	}
+}
+
+int main() {
+
+	//Winsock startup
+	WSAData wsaData;
+	WORD DllVersion = MAKEWORD(2, 2);
+	if (WSAStartup(DllVersion, &wsaData) != 0) {
+		MessageBoxA(NULL, "Winsock startup failed", "Error", MB_OK | MB_ICONERROR);
+		exit(1);
+	}
+
+	SOCKADDR_IN addr;
+	int sizeofaddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(17337);
+	addr.sin_family = AF_INET;
+
+	Connection = socket(AF_INET, SOCK_STREAM, NULL);
+	if (connect(Connection, (SOCKADDR*)&addr, sizeofaddr) != 0) {
+		MessageBoxA(NULL, "MAA FNULLER MAA", "ERROR!", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	std::cout << "Connected" << std::endl;
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientThread, NULL, NULL, NULL);
+
+	char buffer[256];
+	while (true) {
+		std::cin.getline(buffer, sizeof(buffer));
+		send(Connection, buffer, sizeof(buffer), NULL);
+		Sleep(10);
+	}
+
+	system("pause");
+	return 0;
+}
