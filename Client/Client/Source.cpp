3d42d2ba82f847af0d918feca13b3702de563ba6
Bikki <32074350+bgeisl16@users.noreply.github.com>
2017-12-26 00:06:48 +0100

    Update
    Added comments

--- Client/Client/Source.cpp
+++ Client/Client/Source.cpp
@@ -1,53 +1,55 @@
 //Client
+#define _WINSOCK_DEPRECATED_NO_WARNINGS					//Disables API warnings
+#pragma comment(lib, "ws2_32.lib")						//Linker to library
 
-#pragma comment(lib, "ws2_32.lib")
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <WinSock2.h>									//Winsock functions, structures and definitions
+#include <iostream>										//Input/output operations - slower than stdio.h, but more safe
 
-#include <WinSock2.h>
-#include <iostream>
+using namespace std;
 
 SOCKET Connection;
 
-void ClientThread() {
-	char buffer[256];
+char buffer[256];										//Declares a buffer of chars
+
+void ClientThread() {									//Checks for messages from the server
 	while (true) {
-		recv(Connection, buffer, sizeof(buffer), NULL);
-		std::cout << buffer << std::endl;
+		recv(Connection, buffer, sizeof(buffer), NULL);	//Receives messages from the server
+		cout << buffer << endl;							//Prints out the message
 	}
 }
 
 int main() {
-
 	//Winsock startup
-	WSAData wsaData;
-	WORD DllVersion = MAKEWORD(2, 2);
-	if (WSAStartup(DllVersion, &wsaData) != 0) {
-		MessageBoxA(NULL, "Winsock startup failed", "Error", MB_OK | MB_ICONERROR);
-		exit(1);
+	WSAData wsaData;									//The structure contains info about Winsock implementation
+	WORD DllVersion = MAKEWORD(2, 2);					//MAKEWORD is request for version 2.2 of Winsock
+	if (WSAStartup(DllVersion, &wsaData) != 0) {		//Checks for errors in using WS2_32.dll
+		MessageBoxA(NULL, "Winsock startup failed", "Error", MB_OK | MB_ICONERROR); //Shows a window error message
+		exit(1);										//Exits the client
 	}
 
-	SOCKADDR_IN addr;
-	int sizeofaddr = sizeof(addr);
-	addr.sin_addr.s_addr = inet_addr("192.168.38.182");
-	addr.sin_port = htons(17337);
-	addr.sin_family = AF_INET;
+	//Create Socket
+	SOCKADDR_IN addr;									//Specifies a transport address and port for the AF_INET address family
+	int sizeofaddr = sizeof(addr);						//Size of the structure addr
+	addr.sin_addr.s_addr = inet_addr("192.168.38.1");	//IP address
+	addr.sin_port = htons(17337);						//Port
+	addr.sin_family = AF_INET;							//IPv4
 
-	Connection = socket(AF_INET, SOCK_STREAM, NULL);
-	if (connect(Connection, (SOCKADDR*)&addr, sizeofaddr) != 0) {
-		MessageBoxA(NULL, "MAA FNULLER MAA", "ERROR!", MB_OK | MB_ICONERROR);
-		return 0;
+	//Connect client socket to server
+	Connection = socket(AF_INET, SOCK_STREAM, NULL);	//Initialize the SOCKET. SOCK_STREAM = TCP. 
+	if (connect(Connection, (SOCKADDR*)&addr, sizeofaddr) != 0) {	//If no server is found
+		MessageBoxA(NULL, "Failed to connect", "ERROR!", MB_OK | MB_ICONERROR); //Shows a window error message
+		return 0;							
 	}
+	cout << "Connected" << endl;						//If connected to Server
 
-	std::cout << "Connected" << std::endl;
-	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientThread, NULL, NULL, NULL);
-
-	char buffer[256];
+	//Multithreading
+	CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ClientThread, NULL, NULL, NULL); //Creates a thread which runs the ClientThread function
+							
+	//Input from Client
 	while (true) {
-		std::cin.getline(buffer, sizeof(buffer));
-		send(Connection, buffer, sizeof(buffer), NULL);
-		Sleep(10);
+		cin.getline(buffer, sizeof(buffer));			//Enables input from client
+		send(Connection, buffer, sizeof(buffer), NULL);	//Sends the message to the server
+		Sleep(10);										//Sleeps for 10ms
 	}
-
-	system("pause");
 	return 0;
 }
