ca3153ad874ef4122c363fea7dbffd95088ab269
Zekrom9 <nikolajlovig@gmail.com>
2017-12-27 14:30:09 +0100

    Added Comments
    Added comments

--- Server/Server/Server/Source.cpp
+++ Server/Server/Server/Source.cpp
@@ -1,16 +1,16 @@
 //Server
 
-#pragma comment(lib, "ws2_32.lib")
-#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma comment(lib, "ws2_32.lib")						//Tells the linker to add ws2_32.lib to the list of additional library dependencies.
+#define _WINSOCK_DEPRECATED_NO_WARNINGS					//Disables deprecated API warnings.
+#include <WinSock2.h>									//Includes Winsock 2 header files to enable use of the Winsock API.
+#include <iostream>										//Allows the system to read and write to the standard input/output streams.
 
-#include <WinSock2.h>
-#include <iostream>
+using namespace std;									//Allows std namespace qialifiers to be omitted in the code; e.g. so "std::cout" can be written as just "cout".
 
-using namespace std;
+SOCKET Connections[100];								//An array that can store up to 100 sockets.
+int ConnectionCounter = 0;								//An integer used to count the number of clients connected to the server.
 
-SOCKET Connections[100];
-int ConnectionCounter = 0;
-
+//Function called as a secondary thread; handles clients connected to the server.
 void ClientHandlerThread(int index) {
 	char buffer[256];
 	while (true) {
@@ -23,37 +23,42 @@ void ClientHandlerThread(int index) {
 	}
 }
 
+//
 int main() {
+	WSAData wsaData;									//Structure that contains data about Windows Sockets.
+	WORD DllVersion = MAKEWORD(2, 2);					//Creates a 2 byte WORD value for use as a return value from a window procedure.
 
-	//Winsock startup
-	WSAData wsaData;
-	WORD DllVersion = MAKEWORD(2, 2);
+	//Function that takes a WORD value and a data structure pointer. Returns 0 if successful. Returns an error code if not.
 	if (WSAStartup(DllVersion, &wsaData) != 0) {
 		MessageBoxA(NULL, "Winsock startup failed", "Error", MB_OK | MB_ICONERROR);
 		exit(1);
 	}
 
-	SOCKADDR_IN addr;
-	int addrlen = sizeof(addr);
-	addr.sin_addr.s_addr = inet_addr("192.168.38.182");
-	addr.sin_port = htons(17337);
-	addr.sin_family = AF_INET;
+	//Setting socket address values.
+	SOCKADDR_IN addr;									//Specifies endpoint address to which to connect a socket.
+	int addrlen = sizeof(addr);							//Interger value that holds the length of "addr".
+	addr.sin_addr.s_addr = inet_addr("192.168.38.182");	//Specifies the IP address of the socket.
+	addr.sin_port = htons(17337);						//Specifies the port of the socket.
+	addr.sin_family = AF_INET;							//Specifies the internet protocol family.
 
-	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);
-	bind(sListen, (SOCKADDR*)&addr, sizeof(addr));
-	listen(sListen, SOMAXCONN);
+	//Sets up a socket that listens for clients.
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL);//Sets up socket.
+	bind(sListen, (SOCKADDR*)&addr, sizeof(addr));		//Associates address with the socket.
+	listen(sListen, SOMAXCONN);							//Places the socket in a state of listening for incoming connections.
 
+	//Prints text to the console. First thing that the user sees on-screen.
+	cout << "Server is online" << endl;
+
+	//Looped function used to accept new connections. Can take up to 100 clients.
 	SOCKET newConnection;
 	for (int i = 0; i < 100; i++) {
 		newConnection = accept(sListen, (SOCKADDR*)&addr, &addrlen);
-
 		if (newConnection == 0) {
 			cout << "Failed to accept the client's connection" << endl;
 		}
-
 		else {
 			cout << "Client number " << i + 1 << " connected!" << endl;
-			char MOTD[256] = "Welcome! This is the message of the day"; //MAAAAAAA
+			char MOTD[256] = "Welcome! This is the message of the day";
 			send(newConnection, MOTD, sizeof(MOTD), NULL);
 			Connections[i] = newConnection;
 			ConnectionCounter++;
